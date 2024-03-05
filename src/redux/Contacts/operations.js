a8c0726d21fbf8842496c7b51eb162ba01179e98
Makarenko Pavlo <cmertnik1986@gmail.com>
2024-03-05 22:50:47 +0200

    8

--- src/redux/Contacts/operations.js
+++ src/redux/Contacts/operations.js
@@ -29,7 +29,7 @@ export const deleteContact = createAsyncThunk(
     'contacts/deleteContact',
     async (contactId, thunkAPI) => {
         try {
-            const response = await axios.get(`/contacts/${contactId}`);
+            const response = await axios.delete(`/contacts/${contactId}`);
             return response.data;
         } catch (e) {
             return thunkAPI.rejectWithValue(e.message);
